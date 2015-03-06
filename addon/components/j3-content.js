a7dd4928037b1f82c40758c807f291d0b18b0ad5
David Lormor <dlormor@aidt.edu>
2015-03-06 09:36:21 -0600

    Implement J3Content

---
+++ addon/components/j3-content.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['j3-content']
+});
---
+++ app/components/j3-content.js
@@ -0,0 +1,3 @@
+import J3Content from 'j3-ui/components/j3-content';
+
+export default J3Content;
